0af8dcf6976a04e5879db99ac034526909481ee6
KANTHRAJU97 <112216727+KANTHRAJU97@users.noreply.github.com>
2022-08-29 00:29:29 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+package Assign;
+
+public class Sum {
+    public static void main(String[] args)
+    {
+        int a=4;
+        int b=36;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+}
---
+++ Swap.java
@@ -0,0 +1,19 @@
+package Assign;
+
+public class Swap
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("before swaping numbers"+a+" "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("after swaping numbers"+a+" "+b);
+
+
+
+    }
+
+
+}
